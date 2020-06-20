5b9b68393949fb7230f8174f42743a278d79e609
Priscilla Leyva <pleyva.co89@gmail.com>
2020-06-20 05:29:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,11 @@
 def line(katz_deli)
     deli_line = []
-    if katz_deli == empty
+    if katz_deli.empty?
       puts "The line is currently empty."
-end
+    else
+      katz_deli.each.with_index(1) do |name, index|
+        deli_line << "#{index}. #{name}"
+      end
+      puts "The line is currently: #{deli_line.join}"
+    end
+  end
