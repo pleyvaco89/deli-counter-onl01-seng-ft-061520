d8ae10d873301f3997a703eadcec7c92f2c4f70c
Priscilla Leyva <pleyva.co89@gmail.com>
2020-06-20 04:56:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,4 +2,6 @@ def line(katz_deli)
     katz_deli = ["1. Logan", "2. Avi", "3. Spencer"]
     if katz_deli == empty
       puts "The line is currently empty."
+    else 
+      puts "#{katz_deli}"
 end
