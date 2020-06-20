122f9d4f183edcaf69c6209db79bc4bade25db6e
Priscilla Leyva <pleyva.co89@gmail.com>
2020-06-20 05:31:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,28 @@
 def line(katz_deli)
-    deli_line = []
-    if katz_deli.empty?
-      puts "The line is currently empty."
-    else
-      katz_deli.each.with_index(1) do |name, index|
-        deli_line << "#{index}. #{name}"
-      end
-      puts "The line is currently: #{deli_line.join}"
+  deli_line = []
+  if katz_deli.empty?
+    puts "The line is currently empty."
+else 
+  katz_deli.each.with_index(1) do |name, index|
+    deli_line << "#{index}. #{name}"
     end
+  puts "The line is currently: #{deli_line.join" "}"
   end
+
+end
+
+def take_a_number(deli_line, new_customer)
+  # 2 arguments
+  deli_line << new_customer
+    #puts to call the person's name and position
+  puts "Welcome, #{new_customer}. You are number #{deli_line.length} in line."
+
+end
+
+
+def now_serving(katz_deli)
+  if katz_deli.count < 1
+    puts "There is nobody waiting to be served!"
+  else
+        puts "Currently serving #{katz_deli.shift}."
+    end
